cf91cc81212f36b71c83288a8f9a15ca379dbe4f
neonabuko <matheusventureli50@gmail.com>
2024-04-29 04:42:48 -0300

    Create GetScoreFileByNameAsync() and SaveScoreFileAsync()

--- Controllers/ScoreController.cs
+++ Controllers/ScoreController.cs
@@ -10,21 +10,21 @@ namespace SongManager.Controllers;
 public class ScoreController(ScoreRepository scoreRepository) : ControllerBase
 {
     [HttpGet("/scores")]
-    public async Task<IActionResult> GetAllAsync()
+    public async Task<IActionResult> GetAllScoreDataAsync()
     {
         var scores = await scoreRepository.GetAllAsync();
         return Ok(scores);
     }
 
-    [HttpGet("/score/${name}")]
-    public async Task<IActionResult> GetAsync(string name)
+    [HttpGet("/scores/{name}/data")]
+    public async Task<IActionResult> GetScoreDataByNameAsync(string name)
     {
         var score = await scoreRepository.GetByNameAsync(name);
         return Ok(score.AsDto());
     }
 
-    [HttpPost("/scores")]
-    public async Task<IActionResult> SaveAsync(ScoreDto scoreDto)
+    [HttpPost("/scores/data")]
+    public async Task<IActionResult> SaveScoreDataAsync([FromForm] ScoreDto scoreDto)
     {
         Score score = new()
         {
@@ -36,8 +36,25 @@ public class ScoreController(ScoreRepository scoreRepository) : ControllerBase
         return Ok();
     }
 
-    [HttpDelete("/scores/${name}")]
-    public async Task<IActionResult> DeleteAsync(string name)
+    [HttpGet("/scores/{name}")]
+    public IActionResult GetScoreFileByNameAsync(string name)
+    {
+        string scorePath = Path.Combine("/app/scores", name);
+        var fileStream = new FileStream(scorePath, FileMode.Open, FileAccess.Read);
+        return new FileStreamResult(fileStream, "application/vnd.recordare.musicxml+xml");
+    }
+
+    [HttpPost("/scores")]
+    public async Task<IActionResult> SaveScoreFileAsync([FromForm] IFormFile scoreFile)
+    {
+        var scorePath = Path.Combine("/app/scores" + $"/{scoreFile.FileName}");
+        await using var fileStream = new FileStream(scorePath, FileMode.Create, FileAccess.Write);
+        await scoreFile.CopyToAsync(fileStream);
+        return StatusCode(201);
+    }
+
+    [HttpDelete("/scores/{name}")]
+    public async Task<IActionResult> DeleteScoreDataAsync(string name)
     {
         await scoreRepository.DeleteAsync(name);
         return Ok();
